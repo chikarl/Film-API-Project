27c02ef8562db9c1096c9282fe44bc7d526f8add
CHi Karl <kchi8573@gmail.com>
2022-08-04 10:23:14 +0100

    Added the carousel section

--- js/main.js
+++ js/main.js
@@ -1,35 +1,6 @@
 const content = document.querySelector(".content")
 const series = document.querySelector(".series")
 
-const apiRe = "https://api.themoviedb.org/3/trending/movie/day?api_key=cc52d7bc957397a1680d0fc49c0a4492"
-/*fetch(apiRe)
-  .then(response => response.json())
-  .then(data => {
-    let trendingData = data.results
-    console.log(trendingData[0])
-    let movieData = trendingData.map(elem => {
-      // console.log(con);
-      return `<div class="col">
-                    <img src="https://image.tmdb.org/t/p/w500/${elem.poster_path}" alt="">
-                    <a href="https://api.themoviedb.org/3/movie/${elem.id}?api_key=cc52d7bc957397a1680d0fc49c0a4492&language=en-US">
-                    <h5 class="title">${elem.original_title}</h5></a>
-                    <div class="flex">
-                    <p class="date">${elem.release_date}</p>
-                    <div class="rating">
-                      <p> <img src="../image/heart.svg"> <span>${elem.vote_average.toFixed(2)}<span></p>
-                    </div>
-                    </div>
-                    </div>
-                    `
-    }).join('')
-    content.innerHTML = movieData
-  })*/
-// .catch(err => console.error(err));
-// let nums = [1, 2, 3, 4, 5, 6]
-// const movieData = nums.map(num =>
-//   console.log(num))
-
-
 class resultsClass {
   constructor(links, content) {
     this.links = links
@@ -40,9 +11,9 @@ class resultsClass {
       .then(response => response.json())
       .then(data => {
         let trendingData = data.results
-        console.log(trendingData[0])
+
         let movieData = trendingData.map(elem => {
-          // console.log(con);
+
           return `<div class="col">
                     <img src="https://image.tmdb.org/t/p/w500/${elem.poster_path}" alt="">
                     <a href="https://api.themoviedb.org/3/movie/${elem.id}?api_key=cc52d7bc957397a1680d0fc49c0a4492&language=en-US">
@@ -59,10 +30,54 @@ class resultsClass {
         this.content.innerHTML = movieData
       })
   }
-  // console.log(this.links)
+
 }
 
-const trending1 = new resultsClass(apiRe, content)
+const trending1 = new resultsClass("https://api.themoviedb.org/3/trending/movie/day?api_key=cc52d7bc957397a1680d0fc49c0a4492", content)
 trending1.getResults()
 const trending2 = new resultsClass("https://api.themoviedb.org/3/trending/tv/day?api_key=cc52d7bc957397a1680d0fc49c0a4492", series)
 trending2.getResults()
+
+const carousel = document.querySelector(".carousel")
+
+console.log(carousel);
+
+
+fetch("https://api.themoviedb.org/3/trending/tv/day?api_key=cc52d7bc957397a1680d0fc49c0a4492")
+  .then(resp => resp.json())
+  .then(data => {
+    let carousedList = data.results.map(elem => {
+
+      return `<div class="item"> 
+      <h5 class="title">${elem.original_title ? elem.original_title : elem.original_name}</h5></a>
+      
+      </div>
+        `
+    }).join("")
+    const carouselElem = document.createElement("div")
+    carouselElem.innerHTML = carousedList
+
+    carouselElem.addEventListener("load", () => {
+      carouselElem.classList.add("owl-carousel")
+      carouselElem.classList.add("owl-theme")
+      carousel.appendChild(carouselElem)
+
+    })
+  })
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})
